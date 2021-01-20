5926d26001eaddc8099bad8836ceeeb5a0ed2dc7
kevin <fsd83811@gmail.com>
2021-01-20 14:36:50 +0800

    button

--- src/UDP_Send/Form1.Designer.cs
+++ src/UDP_Send/Form1.Designer.cs
@@ -38,97 +38,107 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button9 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(12, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 39);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Send";
+            this.button1.Text = "Back Line";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Btn_Send);
             // 
             // button36
             // 
+            this.button36.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button36.Location = new System.Drawing.Point(12, 255);
             this.button36.Margin = new System.Windows.Forms.Padding(2);
             this.button36.Name = "button36";
-            this.button36.Size = new System.Drawing.Size(204, 32);
+            this.button36.Size = new System.Drawing.Size(204, 39);
             this.button36.TabIndex = 40;
-            this.button36.Text = "back tracking";
+            this.button36.Text = "Back Tracking";
             this.button36.UseVisualStyleBackColor = true;
             this.button36.Click += new System.EventHandler(this.Button36_Click);
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(117, 76);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(99, 39);
             this.button2.TabIndex = 0;
-            this.button2.Text = "Send";
+            this.button2.Text = "Front Line";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Btn_Send2);
             // 
             // button3
             // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(12, 121);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(99, 39);
             this.button3.TabIndex = 0;
-            this.button3.Text = "Send";
+            this.button3.Text = "Back S";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.Btn_Send3);
             // 
             // button4
             // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Location = new System.Drawing.Point(117, 121);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(99, 39);
             this.button4.TabIndex = 0;
-            this.button4.Text = "Send";
+            this.button4.Text = "Front S";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.Btn_Send4);
             // 
             // button5
             // 
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.Location = new System.Drawing.Point(12, 166);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(99, 39);
             this.button5.TabIndex = 0;
-            this.button5.Text = "Send";
+            this.button5.Text = "Back O";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.Btn_Send5);
             // 
             // button6
             // 
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button6.Location = new System.Drawing.Point(117, 166);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(99, 39);
             this.button6.TabIndex = 0;
-            this.button6.Text = "Send";
+            this.button6.Text = "Front O";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.Btn_Send6);
             // 
             // button7
             // 
+            this.button7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button7.Location = new System.Drawing.Point(12, 211);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(99, 39);
             this.button7.TabIndex = 0;
-            this.button7.Text = "Send";
+            this.button7.Text = "Back Home";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.Btn_Send7);
             // 
             // button8
             // 
+            this.button8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button8.Location = new System.Drawing.Point(117, 211);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(99, 39);
             this.button8.TabIndex = 0;
-            this.button8.Text = "Send";
+            this.button8.Text = "Front Home";
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.Btn_Send8);
             // 
@@ -142,12 +152,25 @@
             this.pictureBox1.TabIndex = 41;
             this.pictureBox1.TabStop = false;
             // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(12, 298);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(204, 39);
+            this.button9.TabIndex = 42;
+            this.button9.Text = "Front Tracking";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(235, 295);
+            this.ClientSize = new System.Drawing.Size(235, 349);
+            this.Controls.Add(this.button9);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button36);
             this.Controls.Add(this.button8);
@@ -178,6 +201,7 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button9;
     }
 }
 
